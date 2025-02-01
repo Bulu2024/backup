674c4a7dd056ebb9637011f500d57ebe8ac8d825
Bulu2024 <la.guarapita@gmail.com>
2025-02-01 21:28:48 +0100

    Initial commit

---
+++ backup_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SRC="/data/repository/db/backup/${SCHEMA}"
+DEST="/root/backup_analytics/${SCHEMA}"
+SCHEMA="backup_clickhouse_${DATE}"
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
+
+clickhouse-backup create --config /etc/clickhouse-backup/config.xml --schema "${SCHEMA}"
+cp -r "${SRC}" "${DEST}"
+
+cd /root/backup_analytics || { echo "Directory not found"; exit 1; }
+git add .
+git commit -m "Backup ${SCHEMA}"
+git push origin main
