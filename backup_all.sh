63f4b3d7132c5f370a5bd4760419b519beaea776
Bulu2024 <la.guarapita@gmail.com>
2025-02-01 21:29:44 +0100

    Backup backup_clickhouse_

--- backup_all.sh
+++ backup_all.sh
@@ -5,7 +5,7 @@ DEST="/root/backup_analytics/${SCHEMA}"
 SCHEMA="backup_clickhouse_${DATE}"
 DATE=$(date +'%Y-%m-%d_%H-%M-%S')
 
-clickhouse-backup create --config /etc/clickhouse-backup/config.xml --schema "${SCHEMA}"
+clickhouse-backup create --config /etc/clickhouse-backup/config.yml --schema "${SCHEMA}"
 cp -r "${SRC}" "${DEST}"
 
 cd /root/backup_analytics || { echo "Directory not found"; exit 1; }
