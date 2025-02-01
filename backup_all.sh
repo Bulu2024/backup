a249af4eadd08aec7f260a5af762df4051eebbc7
Bulu2024 <la.guarapita@gmail.com>
2025-02-01 22:43:49 +0100

    Initial commit

---
+++ backup_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
+SRC_DB="/data/repository/db/backup/${DATE}_backup_clickhouse"
+SRC_SW="/opt/analytics"
+DEST="/srv/backup/backup_data/"
+
+# Clickhouse
+clickhouse-backup create --config /etc/clickhouse-backup/config.yml --schema "${DATE}_backup_clickhouse"
+tar -czvf "${DEST}${DATE}_backup_clickhouse.tar.gz" "${SRC_DB}"
+
+# App
+SRC_SW="/opt/analytics"
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
+ARCHIVE="${DEST}${DATE}_analytics_app_backup.tar.gz"
+export GZIP=-9
+tar -czvf "$ARCHIVE" \
+    --exclude='.git' \
+    --exclude='*.csv' \
+    --exclude='*.csv.gz' \
+    --exclude='*.tar.gz' \
+    --exclude='*.log' \
+    --exclude='*.log.*' \
+    -C "$SRC_SW" .
+
+echo "Backup complete: $ARCHIVE"
+
+# Git
+cd "$DEST.." || { echo "Directory not found"; exit 1; }
+git add .
+git commit -m "Backup ${DATE}"
+git push origin main
