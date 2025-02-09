23d5e9ee580495954cc7cf90602c247eab398f9b
Bulu2024 <la.guarapita@gmail.com>
2025-02-09 04:30:01 +0100

    Backup 2025-02-09_04-30-01

--- backup_all.sh
+++ backup_all.sh
@@ -37,10 +37,10 @@ DATE_TIME=$(date +%Y%m%d_%H%M%S)
 LOCAL_BACKUP_DIR="/data/repository/backup"
 REMOTE_DEST="root@3.1.1.2:/data/backup"
 BACKUP_FILE="${HOST}_${DATE_TIME}_server_backup.tsbak"
-CONFIG_FILE="${HOST}_${DATE_TIME}_server_config.json"
+CONFIG_FILE="${LOCAL_BACKUP_DIR}/${HOST}_${DATE_TIME}_server_config.json"
 
 tsm maintenance backup -f "${BACKUP_FILE}" --ignore-prompt
 tsm settings export -f "${CONFIG_FILE}"
 
 scp "${LOCAL_BACKUP_DIR}/${BACKUP_FILE}" "${REMOTE_DEST}"
-scp "${LOCAL_BACKUP_DIR}/${CONFIG_FILE}" "${REMOTE_DEST}"
+scp "${CONFIG_FILE}" "${REMOTE_DEST}"
